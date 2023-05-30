be9f561f281869510a5504d1aeeff47283f989f9
loopguard404 <46474603+loopguard404@users.noreply.github.com>
2023-05-30 12:50:59 +0700

    Update ibm.py

--- ibm.py
+++ ibm.py
@@ -1,35 +1,103 @@
 #!/usr/bin/python3
  
+import sys
 import paramiko
- 
+import subprocess
+
 target = 'ip'
 login = 'user'
 password = 'pass'
 command = 'lssystem -delim \,'
  
-def ssh_exec(command, target, user, password, port=22):
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
- 
-    try:
-        client.connect(target, username=user, password=password, port=port)
-    except:
-        print('FATAL: Unable to log in')
-        exit(1)
- 
-    stdin, stdout, stderr = client.exec_command(command)
- 
-    error = stderr.read()
-    if error:
-        error = error.decode('US-ASCII')
-        print('Error running the command:{}'.format(error))
-        client.close()
-        return 0
- 
-    data = stdout.read()
-    client.close()
- 
-    return data.decode('US-ASCII')
- 
-lssystem = ssh_exec(command, target, login, password)
-print(lssystem)
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(hostname=host, username=user, password=secret, port=port)
+stdin, stdout, stderr = client.exec_command('lsdrive')
+lsdrive = stdout.read()
+lsdrive = lsdrive.decode('UTF-8')
+file = '/etc/zabbix/scripts/ibm/result/10.0.0.180/lsdrive.txt'
+with open(file, 'w') as file:
+    file.write(lsdrive)
+
+
+#stdin, stdout, stderr = client.exec_command('lssystemstats')
+#lssystemstats = stdout.read()
+#lssystemstats = lssystemstats.decode('UTF-8')
+#file = '/etc/zabbix/scripts/ibm/result/10.0.0.180/lssystemstats.txt'
+#with open(file, 'w') as file:
+#    file.write(lssystemstats)
+
+
+stdin, stdout, stderr = client.exec_command('lsarray')
+lsarray = stdout.read()
+lsarray = lsarray.decode('UTF-8')
+file = '/etc/zabbix/scripts/ibm/result/10.0.0.180/lsarray.txt'
+with open(file, 'w') as file:
+    file.write(lsarray)
+
+
+stdin, stdout, stderr = client.exec_command('lsenclosurebattery')
+lsenclosurebattery = stdout.read()
+lsenclosurebattery = lsenclosurebattery.decode('UTF-8')
+file = '/etc/zabbix/scripts/ibm/result/10.0.0.180/lsenclosurebattery.txt'
+with open(file, 'w') as file:
+    file.write(lsenclosurebattery)
+
+
+stdin, stdout, stderr = client.exec_command('lshost')
+lshost = stdout.read()
+lshost = lshost.decode('UTF-8')
+file = '/etc/zabbix/scripts/ibm/result/10.0.0.180/lshost.txt'
+with open(file, 'w') as file:
+    file.write(lshost)
+
+
+
+stdin, stdout, stderr = client.exec_command('lsnodecanisterstats')
+lsnodecanisterstats = stdout.read()
+lsnodecanisterstats = lsnodecanisterstats.decode('UTF-8')
+file = '/etc/zabbix/scripts/ibm/result/10.0.0.180/lsnodecanisterstats.txt'
+with open(file, 'w') as file:
+    file.write(lsnodecanisterstats)
+
+
+stdin, stdout, stderr = client.exec_command('lsmdiskgrp')
+lsmdiskgrp = stdout.read()
+lsmdiskgrp = lsmdiskgrp.decode('UTF-8')
+file = '/etc/zabbix/scripts/ibm/result/10.0.0.180/lsmdiskgrp.txt'
+with open(file, 'w') as file:
+    file.write(lsmdiskgrp)
+
+
+
+stdin, stdout, stderr = client.exec_command('lsportip')
+lsportip = stdout.read()
+lsportip = lsportip.decode('UTF-8')
+file = '/etc/zabbix/scripts/ibm/result/10.0.0.180/lsportip.txt'
+with open(file, 'w') as file:
+    file.write(lsportip)
+
+
+stdin, stdout, stderr = client.exec_command('lssevdiskcopy')
+lssevdiskcopy = stdout.read()
+lssevdiskcopy = lssevdiskcopy.decode('UTF-8')
+file = '/etc/zabbix/scripts/ibm/result/10.0.0.180/lssevdiskcopy.txt'
+with open(file, 'w') as file:
+    file.write(lssevdiskcopy)
+
+
+stdin, stdout, stderr = client.exec_command('lsvdisk')
+lsvdisk = stdout.read()
+lsvdisk = lsvdisk.decode('UTF-8')
+file = '/etc/zabbix/scripts/ibm/result/10.0.0.180/lsvdisk.txt'
+with open(file, 'w') as file:
+    file.write(lsvdisk)
+
+
+stdin, stdout, stderr = client.exec_command('lsvdiskanalysis')
+lsvdiskanalysis = stdout.read()
+lsvdiskanalysis = lsvdiskanalysis.decode('UTF-8')
+file = '/etc/zabbix/scripts/ibm/result/10.0.0.180/lsvdiskanalysis.txt'
+with open(file, 'w') as file:
+    file.write(lsvdiskanalysis)
+client.close()
